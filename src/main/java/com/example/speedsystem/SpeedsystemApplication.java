a90e0702385388f1dbfec6f4b0d77f3d76b8cd6d
Diego6624 <juan2525.2004@gmail.com>
2025-11-11 19:33:19 -0500

    Creación del proyecto

---
+++ src/main/java/com/example/speedsystem/SpeedsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.speedsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/speedsystem/SpeedsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speedsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
